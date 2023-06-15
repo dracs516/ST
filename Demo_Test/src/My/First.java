bc0b1ee33c7364f167d652e5e37ca483f76bf7fe
dracs516 <jha.abhay02@gmail.com>
2023-06-15 10:47:47 +0530

    selenium not working

---
+++ Demo_Test/src/My/First.java
@@ -0,0 +1,21 @@
+package My;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/dracs02/:root/Coding/ST/chromedriver_mac_arm64/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("http://www.google.com/");
+		driver.findElement(By.id("APjFqb")).sendKeys("BMS College of Engineering");
+		driver.findElement(By.name("btnK")).click();
+	}
+
+}
